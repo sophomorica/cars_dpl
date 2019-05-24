72083c2be45dea2fcaa26a1aedeb8e378573c994
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-24 11:24:09 -0600

    used shouldaMatcher

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,19 @@
-class Car < ApplicationRecord
-end
+class Car < ActiveRecord::Base
+  def self.by_model
+  end
+  
+  def self.by_make
+  end
+  
+  def self.by_price(high = false)
+  end
+  
+  def paint(color)
+  end
+  
+  def info
+  end
+  
+  def honk
+  end
+ end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    # it 'has a make' do
+    #   make = "Toyota"
+    #   car = Car.create(make:make)
+    #   expect(car.make).to eq(make)
+    # end
+    it { should respond_to :make }
+    it { should respond_to :model }
+    it { should respond_to :age }
+    it { should respond_to :price }
+    it { should respond_to :mileage }
+    it { should respond_to :color }
+    it { should respond_to :interior }
+  end
 end
